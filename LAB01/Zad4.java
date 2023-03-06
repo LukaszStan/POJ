de4789e02ec8a7f9ac5fcaff9e067607464a9d35
LukaszStan <120740902+LukaszStan@users.noreply.github.com>
2023-03-06 11:04:52 +0100

    Update Zad4.java

--- LAB01/Zad4.java
+++ LAB01/Zad4.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 public class Zad4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
